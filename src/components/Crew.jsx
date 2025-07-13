03853553e957ff98a5b92f618cc21cfde75d1a71
Hack3 <oliver@Hac3r>
2025-07-13 02:22:42 +0200

    icons and pics rdy

--- src/components/Crew.jsx
+++ src/components/Crew.jsx
@@ -128,7 +128,11 @@ const Crew = () => {
               
 
               {/* Member Image */}
-              <div className="member-image">
+              <div className="member-image" style={{
+                '--filla-position': 'center',
+                '--pilla-position': 'center 60%',
+                '--zkittlez-position': 'center'
+              }}>
                 <motion.img 
                   src={`${import.meta.env.BASE_URL}${member.photo}`}
                   alt={`${member.stage} - ${member.role}`}
@@ -328,16 +332,9 @@ const SpotifyIcon = () => (
     width="18" 
     height="18" 
     viewBox="0 0 24 24" 
-    fill="none"
-    stroke="currentColor"
-    strokeWidth="2.5"
-    strokeLinecap="round"
-    strokeLinejoin="round"
+    fill="currentColor"
   >
-    <circle cx="12" cy="12" r="10"/>
-    <path d="M8 14.5c1.5-1 4-1 5.5 0"/>
-    <path d="M8 10.5c2.5-1.5 6.5-1.5 9 0"/>
-    <path d="M8 6.5c3.5-2 8.5-2 12 0"/>
+    <path d="M12 0C5.4 0 0 5.4 0 12s5.4 12 12 12 12-5.4 12-12S18.66 0 12 0zm5.521 17.34c-.24.359-.66.48-1.021.24-2.82-1.74-6.36-2.101-10.561-1.141-.418.122-.859-.179-.979-.599-.122-.421.18-.861.599-.979 4.56-1.021 8.52-.6 11.64 1.32.42.18.479.659.322 1.159zm1.44-3.3c-.301.42-.841.6-1.262.3-3.239-1.98-8.159-2.58-11.939-1.38-.479.12-1.02-.12-1.14-.6-.12-.48.12-1.021.6-1.141C9.6 9.9 15 10.561 18.72 12.84c.361.181.54.78.241 1.2zm.12-3.36C15.24 8.4 8.82 8.16 5.16 9.301c-.6.179-1.2-.181-1.38-.721-.18-.601.18-1.2.72-1.381 4.26-1.26 11.28-1.02 15.721 1.621.539.3.719 1.02.42 1.56-.299.421-1.02.599-1.559.3z"/>
   </svg>
 )
 
